1ebf199f8b5b14f9e7dd45ee969f72689489d39f
brunojcbrg-cloud <brunojcbrg-cloud@users.noreply.github.com>
2026-09-21 11:46:07 -0300

    fix: compilar popup de sugestoes no Android

--- app/src/main/java/io/github/wiiznokes/gitnote/ui/screen/app/edit/WikilinkEditor.kt
+++ app/src/main/java/io/github/wiiznokes/gitnote/ui/screen/app/edit/WikilinkEditor.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.BasicTextField
